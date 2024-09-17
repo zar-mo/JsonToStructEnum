655152b53ca2afe39645d0d199dd99e0bd931644
Abouzar <abouzarmoradian1@gmail.com>
2024-09-17 11:30:06 -0400

    Initial Commit

---
+++ JsonToStructEnum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JsonToStructEnum
+//
+//  Created by Abouzar Moradian on 9/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
